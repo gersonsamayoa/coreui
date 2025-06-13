0ef35feecef89c52193703021ced33638fee47fd
Gerson Samayoa <gersonsamayoa@gmail.com>
2025-06-12 23:56:50 -0600

    GS: Ya pagineo en tabla con componente de angular materializer

--- src/app/views/usuarios/usuarios.component.ts
+++ src/app/views/usuarios/usuarios.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TableDirective } from '@coreui/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { HttpClient } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { CommonModule } from '@angular/common';
 
 @Component({
   templateUrl: 'usuarios.component.html',
   styleUrls: ['usuarios.component.scss'],
-  imports: [CommonModule, TableDirective],
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule]
 })
 export class UsuariosComponent implements OnInit {
+  displayedColumns: string[] = ['menuAdminId', 'nombre', 'user', 'rol', 'estadoNombre'];
+  dataSource = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private http: HttpClient) {}
- usuarios: any[] = []; // Array to hold user data
 
   ngOnInit(): void {
-    this.getData();
-  }
-
-  getData() {
     this.http.get('https://4i3jzllr1l.execute-api.us-east-1.amazonaws.com/dev/usuario').subscribe({
-      next: (data: any) => {    
-        this.usuarios = data?.data?.usuarios; 
-        console.log('Usuarios:', this.usuarios);
-      },  
+      next: (data: any) => {
+        this.dataSource.data = data?.data?.usuarios || [];
+        this.dataSource.paginator = this.paginator;
+         console.log('Usuarios:', data);
+      },
       error: (error) => {
         console.error('Error:', error);
       }
     });
-  } 
-}
+  }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+}
