c2f1983c34a51a439ff7cc660189b991bb8c31d9
Gerson Samayoa <gersonsamayoa@gmail.com>
2025-06-16 14:15:42 -0600

    GS: tabla con paginado y filtro a piesito

--- src/app/views/usuarios/usuarios.component.ts
+++ src/app/views/usuarios/usuarios.component.ts
@@ -8,15 +8,31 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { CommonModule } from '@angular/common';
 
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective,PageItemDirective, PageLinkDirective, PaginationComponent } from '@coreui/angular';
+
+import { FormsModule } from '@angular/forms';
+
+
 @Component({
   templateUrl: 'usuarios.component.html',
   styleUrls: ['usuarios.component.scss'],
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule,      TableDirective, FormsModule,PageItemDirective, PageLinkDirective, PaginationComponent    ]
 })
 export class UsuariosComponent implements OnInit {
   displayedColumns: string[] = ['menuAdminId', 'nombre', 'user', 'rol', 'estadoNombre'];
   dataSource = new MatTableDataSource<any>([]);
+  usuarios: any[] = [];
+  paginaActual = 1;
+  usuariosPorPagina = 3;
+  
+  
+  filtros = {
+  estadoNombre: '',
+  roles: '',
+  user: '',
+  usuarioNombre: ''
+};
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -27,6 +43,7 @@ export class UsuariosComponent implements OnInit {
     this.http.get('https://4i3jzllr1l.execute-api.us-east-1.amazonaws.com/dev/usuario').subscribe({
       next: (data: any) => {
         this.dataSource.data = data?.data?.usuarios || [];
+        this.usuarios = this.dataSource.data;
         this.dataSource.paginator = this.paginator;
          console.log('Usuarios:', data);
       },
@@ -40,4 +57,34 @@ export class UsuariosComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
+  get usuariosPaginados() {
+  const inicio = (this.paginaActual - 1) * this.usuariosPorPagina;
+  return this.usuariosFiltrados.slice(inicio, inicio + this.usuariosPorPagina);
+}
+
+
+get usuariosFiltrados() {
+  return this.usuarios.filter(usuario =>
+    (!this.filtros.estadoNombre || usuario.estadoNombre?.toLowerCase().includes(this.filtros.estadoNombre.toLowerCase())) &&
+    (!this.filtros.roles || usuario.roles?.toLowerCase().includes(this.filtros.roles.toLowerCase())) &&
+    (!this.filtros.user || usuario.user?.toLowerCase().includes(this.filtros.user.toLowerCase())) &&
+    (!this.filtros.usuarioNombre || usuario.usuarioNombre?.toLowerCase().includes(this.filtros.usuarioNombre.toLowerCase()))
+  );
+}
+
+get totalPaginas() {
+  return Math.ceil(this.usuariosFiltrados.length / this.usuariosPorPagina);
+}
+
+get totalPaginasArray() {
+  return Array.from({ length: this.totalPaginas }, (_, i) => i + 1);
+}
+
+cambiarPaginaCore(nuevaPagina: number) {
+  if (nuevaPagina < 1 || nuevaPagina > this.totalPaginas) return;
+  this.paginaActual = nuevaPagina;
+}
+
+
 }
